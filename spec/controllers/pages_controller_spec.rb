058d089de53f00ec9d9a5e74a34512442e558d6c
Pau Haro Negre <pauharo@gmail.com>
2010-07-31 01:22:35 +0200

    Error in base_title spec variable

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,7 +4,7 @@ describe PagesController do
   integrate_views
 
   before(:each) do
-    base_title = "Ruby on Rails Tutorial Sample App"
+    @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
   #Delete these examples and add some real ones
